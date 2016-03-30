6c84631d259d1bc12bfae35b87e6dc422f845050
cruz02148 <cruz02148@gmail.com>
2016-03-29 23:42:37 -0400

    Solved a few problems using recursion

--- src/recursion.js
+++ src/recursion.js
@@ -5,11 +5,26 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
+	if (n < 0) {
+		return null
+	} else if (n === 0) {
+		return 1;
+	} else {
+		return n * factorial(n-1);
+	}
 };
 
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+	var newArr = array.slice(0);
+	if (newArr.length === 0) {
+		return 0;
+	} else if (newArr.length === 1) {
+		return array[0];
+	} else {
+		return newArr.pop() + sum(newArr);
+	}
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -19,6 +34,15 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+	if (n < 0) {
+		return isEven(-n);
+	} else if (n === 0) {
+		return true;
+	} else if (n === 1) {
+		return false;
+	} else {
+		return isEven(n-2);
+	}
 };
 
 // 5. Sum all integers below a given integer.
